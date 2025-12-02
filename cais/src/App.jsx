b0e47505abb224ecc0943e006baa2869a856e48a
Chun <beerdsenc@gmail.com>
2025-12-02 14:28:07 +0100

    added routes and empty page files

--- cais/src/App.jsx
+++ cais/src/App.jsx
@@ -1,6 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage.jsx";
 import Login from "./pages/Login.tsx";
+import ProductList from "./pages/ProductList.tsx";
+import ProductDetails from "./pages/ProductDetail.tsx";
+import UserProfilePage from "./pages/UserProfilePage.tsx";
+import CartPage from "./pages/CartPage.tsx";
 import { PrivateRoute } from "./pages/PrivateRoute.js";
 
 function App() {
@@ -12,7 +16,10 @@ function App() {
       {/* private routes go here. if user is not logged in return to login page */}
       <Route element={<PrivateRoute />}>
       {/* all routes go here after the login page */}
-      
+      <Route path="/products" element={<ProductList />} />
+      <Route path="/products/:productId" element={<ProductDetails />} />
+      <Route path="/profile/:userId" element={<UserProfilePage />} />
+      <Route path="/:userId/cart" element={<CartPage />} />
       </Route>
     </Routes>
   );
