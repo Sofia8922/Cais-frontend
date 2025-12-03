a8f9b98a60b6107fa6ff05459adedabc6477715c
Chun <beerdsenc@gmail.com>
2025-12-03 11:14:58 +0100

    added basic productdetail and sililar product component

--- cais/src/pages/ProductDetail.tsx
+++ cais/src/pages/ProductDetail.tsx
@@ -1,8 +1,45 @@
+import SimilarProducts from "../components/SimilarProducts";
 
 
 export default function ProductDetail() {
     return (
-        <>
-        </>
+        <div>
+            {/* navbar component here */}
+
+            <div className="product-grid">
+                {/* left block */}
+                <div>
+                    <div>
+                        <h2>Title here</h2>
+                        <span>stock here</span>
+                    </div>
+                    {/* img here */}
+                </div>
+
+                {/* center block */}
+                <div>
+                    <div>
+                        <h2>cost here</h2>
+                        {/* admin button to edit here */}
+                    </div>
+                    <p>decription</p>
+
+                    <div>
+                        <h2>category here</h2>
+                        <div>
+                            {/* amount input here */}
+                            <button>Add to cart</button>
+                            {/* maybe a saved button next to the add to cart button too? */}
+                        </div>
+                    </div>
+                </div>
+
+                {/* right block */}
+                <div>
+                    <h1>Similar products:</h1>
+                    <SimilarProducts />
+                </div>
+            </div>
+        </div>
     );
 }
