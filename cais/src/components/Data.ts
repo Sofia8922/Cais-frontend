3faad09d86a83b21c2268b3e582ac196df8376b5
Chun <beerdsenc@gmail.com>
2025-12-04 13:43:55 +0100

    dummy data

---
+++ cais/src/components/Data.ts
@@ -0,0 +1,114 @@
+export const data = [
+  {
+    "name": "Brie",
+    "description": "1 kilo",
+    "price": 3.45,
+    "stock": 4,
+    "imageLink": "broken link here",
+    "subcategory": {
+      "name": "Other cheeses",
+      "category": {
+        "name": "Cheese"
+      }
+    },
+    "purchases": []
+  },
+  {
+    "name": "La Vache Qui Rit",
+    "description": "",
+    "price": 5,
+    "stock": 5,
+    "imageLink": "",
+    "subcategory": {
+      "name": "Other cheeses",
+      "category": {
+        "name": "Cheese"
+      }
+    },
+    "purchases": []
+  },
+  {
+    "name": "La Vache Qui Rit (Vegan)",
+    "description": "",
+    "price": 4.5,
+    "stock": 2,
+    "imageLink": "https://www.lavachequirit.ca/wp-content/uploads/2024/01/plant-product-single-768x768.png",
+    "subcategory": {
+      "name": "Other cheeses",
+      "category": {
+        "name": "Cheese"
+      }
+    },
+    "purchases": []
+  },
+  {
+    "name": "Mozzarella",
+    "description": "italiano",
+    "price": 4.65,
+    "stock": 0,
+    "imageLink": "https://1.bp.blogspot.com/-YJ0fMjTkhk8/Uuejy9zgm2I/AAAAAAAAGFQ/DulK9iYhezc/s1600/Mozzarella.jpg",
+    "subcategory": {
+      "name": "Italian cheeses",
+      "category": {
+        "name": "Cheese"
+      }
+    },
+    "purchases": []
+  },
+  {
+    "name": "Standard Cheese Slicer",
+    "description": "The best one",
+    "price": 20,
+    "stock": 0,
+    "imageLink": "",
+    "subcategory": {
+      "name": "Basic slicers",
+      "category": {
+        "name": "Slicer"
+      }
+    },
+    "purchases": []
+  },
+  {
+    "name": "Gouda",
+    "description": "1 kilo",
+    "price": 10.4,
+    "stock": 7,
+    "imageLink": "https://president-professionnel.com/wp-content/uploads/2024/02/gouda-wheel-president.png",
+    "subcategory": {
+      "name": "Dutch cheeses",
+      "category": {
+        "name": "Cheese"
+      }
+    },
+    "purchases": [
+      {
+        "amount": 2,
+        "status": "PROCESSING"
+      },
+      {
+        "amount": 1,
+        "status": "PROCESSING"
+      }
+    ]
+  },
+  {
+    "name": "Mozzarella Tradizionale",
+    "description": "",
+    "price": 6,
+    "stock": 496,
+    "imageLink": "https://1.bp.blogspot.com/-YJ0fMjTkhk8/Uuejy9zgm2I/AAAAAAAAGFQ/DulK9iYhezc/s1600/Mozzarella.jpg",
+    "subcategory": {
+      "name": "Italian cheeses",
+      "category": {
+        "name": "Cheese"
+      }
+    },
+    "purchases": [
+      {
+        "amount": 4,
+        "status": "PROCESSING"
+      }
+    ]
+  }
+]
