98f14d3d69f6c45d947bf4091e51ab5f1c2e8b41
Chun <beerdsenc@gmail.com>
2025-12-04 13:43:06 +0100

    added test b route

--- cais/src/App.jsx
+++ cais/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage.jsx";
-import Login from "./pages/Login.tsx";
+// import Login from "./pages/Login.tsx";
 import ProductList from "./pages/ProductList.tsx";
 import ProductDetail from "./pages/ProductDetail.tsx";
 import UserProfilePage from "./pages/UserProfilePage.tsx";
@@ -11,13 +11,14 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
-      <Route path="/login" element={<Login />} />
+      {/* <Route path="/login" element={<Login />} /> */}
 
       {/* private routes go here. if user is not logged in return to login page */}
       {/* <Route element={<PrivateRoute />}> */}
       {/* all routes go here after the login page */}
       <Route path="/products" element={<ProductList />} />
       <Route path="/a" element={<ProductDetail />} />
+      <Route path="/b" element={<UserProfilePage />} />
       <Route path="/products/:productId" element={<ProductDetail />} />
       <Route path="/profile/:userId" element={<UserProfilePage />} />
       <Route path="/:userId/cart" element={<CartPage />} />
