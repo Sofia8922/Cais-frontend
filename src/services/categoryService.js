9cc2573cb989af0245e42493b0b6ab4eb634cccc
Chun <beerdsenc@gmail.com>
2025-12-10 15:19:23 +0100

    added update and delete to category and subcategory api

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -4,6 +4,12 @@ export const CategoryService = {
     createCategory: (dto) =>
         api.post("/categories", dto),
 
+    updateCategory: (id, dto) =>
+        api.put(`/categories/${id}`, dto),
+
+    deleteCategory: (id) => 
+        api.delete(`/categories/${id}`),
+
     getAllCategories: () =>
         api.get("/categories"),
 }
--- src/services/subcategoriesService.js
+++ src/services/subcategoriesService.js
@@ -4,6 +4,12 @@ export const SubcategoriesService = {
     createSubcategory: (dto) =>
         api.post("/subcategories", dto),
 
+    updateSubcategory: (id, dto) => 
+        api.put(`/sibcategories/${id}`, dto),
+
+    deleteSubcategory: (id) => 
+        api.delete(`/subcategories/${id}`),
+
     getAllSubcategories: () =>
         api.get("subcategories"),
 }
